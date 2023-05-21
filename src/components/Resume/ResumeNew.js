825b2e00fa83f010cd0bfa2d466b6748f3364da0
amirreza-ashrafi-dev <aa090818@yahoo.com>
2023-05-21 08:17:15 +0430

    edit pdf url

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://mstorage2.mtabazi.com/records/files/uploads/documents/3cbb52fc-9b2c-49e2-bb81-b17b25ba4e27.pdf?requester=3136332e3137322e3137342e313338&resource=753a32313933343535&from=66696c655f75706c6f61646572&X-Amz-Content-Sha256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=oetSom924MQvtCRu9N6vvQ4bt8cG9HSpRnxWU756SyWy5NVt6nTpmfA9aV2b%2F20230520%2F%2Fs3%2Faws4_request&X-Amz-Date=20230520T161657Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1800&X-Amz-Signature=527d59b4a5c3629272e15c131270c18f365bc1c8fdb8213abef6ec6a7879292f";
+  "https://mstorage2.mtabazi.com/records/files/uploads/documents/1c9e7dd3-a2a3-422a-a5c3-4081fd484008.pdf?requester=3136332e3137322e3137342e313338&resource=753a32313933343535&from=66696c655f75706c6f61646572&X-Amz-Content-Sha256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=oetSom924MQvtCRu9N6vvQ4bt8cG9HSpRnxWU756SyWy5NVt6nTpmfA9aV2b%2F20230520%2F%2Fs3%2Faws4_request&X-Amz-Date=20230520T164551Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1800&X-Amz-Signature=c24d880484c453cecad9993ebfa8616691efb425fd785276131dec574721bd26";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
