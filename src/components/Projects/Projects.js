8d985451ebc65c70de7b07be980d6898bb8163ea
amirreza-ashrafi-dev <aa090818@yahoo.com>
2023-05-21 07:31:03 +0430

    first commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import image1 from '../../Assets/1.png';
+import image2 from '../../Assets/2.png';
+import image3 from '../../Assets/3.png';
+import image4 from '../../Assets/4.png';
+import image5 from '../../Assets/Projects/react-dev.jpg';
+import image6 from '../../Assets/Projects/atbin5.PNG';
+import image7 from '../../Assets/Projects/sajafPic1.PNG';
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image7}
+              isBlog={false}
+              title="golestan"
+            // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+            // ghLink="https://github.com/reactjs/react.dev"
+            // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image6}
+              isBlog={false}
+              title="atbin"
+            // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+            // ghLink="https://github.com/reactjs/react.dev"
+            // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image1}
+              isBlog={false}
+              title="Travel agency"
+              // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/amirreza-ashrafi-dev/travel-agency"
+            // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image2}
+              isBlog={false}
+              title="Travel guide"
+              // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/zero-to-mastery/travel-guide"
+            // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image3}
+              isBlog={false}
+              title="ecommerce api"
+              // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/amirreza-ashrafi-dev/Ecommerce-node"
+            // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image4}
+              isBlog={false}
+              title="food ecommerce"
+              // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/amirreza-ashrafi-dev/foodecommerce"
+            // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={image5}
+              isBlog={false}
+              title="react doc"
+              // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/reactjs/react.dev"
+            // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
